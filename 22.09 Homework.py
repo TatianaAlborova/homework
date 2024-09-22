dce4d51f9a9283da9aceb00c1c79ffd5f50ecb54
TatianaAlborova <tatanaalborova331@gmail.com>
2024-09-22 23:21:08 +0300

    Add files via upload

---
+++ 22.09 Homework.py
@@ -0,0 +1,5 @@
+Amount_of_completed_tasks = 12
+Amount_of_hours_spent = 1.5
+Course_name = 'Python'
+Amount_of_time_spent_on_one_task = Amount_of_hours_spent / Amount_of_completed_tasks
+print('Курс:', Course_name, ',' , 'всего задач:', Amount_of_completed_tasks, ',', 'затрачено часов:', Amount_of_hours_spent, ',', 'среднее время выполнения', Amount_of_time_spent_on_one_task, 'часа')
