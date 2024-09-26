56ac553610dbc05dbc0d59b4f3f506e8fb9e63ff
TatianaAlborova <tatanaalborova331@gmail.com>
2024-09-26 11:01:12 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,6 @@
+immutable_var = 1, True, 'lesson', 2.5
+print(immutable_var)
+#кортеж не поддерживает обращения по элементам, поэтому является неизменным
+mutable_list = [1, True, 'lesson', 2.5]
+mutable_list [2] = 'homework'
+print(mutable_list)
