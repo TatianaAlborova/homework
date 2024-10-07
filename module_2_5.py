10d30ad70a1e427cb5a7006520d2ff615b67744e
TatianaAlborova <tatanaalborova331@gmail.com>
2024-10-07 19:08:30 +0300

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,18 @@
+#  Функция get_matrix с параметрами n, m, value
+def get_matrix (n, m, value):
+    # Создаем матрицу n x m, заполненную значениями value
+    matrix = []
+    if n > 0 and m > 0:
+        for i in range (n):
+            matrix.append([])
+            for j in range (m):
+                matrix[i].append(value)
+        return matrix
+    else:
+        return matrix
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
